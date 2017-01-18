fc726d3fc9927751f424ffee801c8f122f3945a2
user1 <user1@user1-PC>
2017-01-18 12:39:59 +0900

    jjj

--- center/src/center/action/SuperAction.java
+++ center/src/center/action/SuperAction.java
@@ -3,6 +3,7 @@ package center.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public interface SuperAction {
-	public String executeAction(HttpServletRequest request, HttpServletResponse response) ;
+	public String executeAction(HttpServletRequest request, HttpServletResponse response) ; 
 }
